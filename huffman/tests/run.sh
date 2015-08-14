1e0df7bc749f1ccbf423c174351559f7f5c804ff
Troy D. Hanson <tdh@tkhanson.net>
2015-08-13 21:05:47 -0400

    test

--- huffman/tests/run.sh
+++ huffman/tests/run.sh
@@ -10,11 +10,17 @@ set -x
 H=../huff
 if [ ! -x $H ]; then echo "run make in parent directory first"; fi
 
+# normal encode/decode
 $H -e -i raw -o cwh
 $H -d -i cwh -o bak
 diff raw bak
 
+# encode (saving the code book externally), decode using it
 $H -e -i raw -o cwo -C cbk
 $H -d -i cwo -o bak -c cbk
 diff raw bak
 
+# encode (using the code book from above), decode using it
+$H -e -i raw -o cwo -c cbk
+$H -d -i cwo -o bak -c cbk
+diff raw bak
