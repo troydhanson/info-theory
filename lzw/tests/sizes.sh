9810d7dd3ae91485161d9a4cf85c7889840e0024
Troy D. Hanson <tdh@tkhanson.net>
2015-08-15 15:21:27 -0400

    wip

--- lzw/tests/sizes.sh
+++ lzw/tests/sizes.sh
@@ -9,16 +9,18 @@
 
 #V=valgrind
 H=../lzw
-if [ ! -x $H ]; then echo "run make in parent directory first"; fi
+if [ ! -x $H ]; then echo "run make in parent directory first"; exit -1; fi
 
-#dd if=/dev/urandom of=/tmp/rand bs=1 count=100000 >/dev/null 2>&1
+echo -n "generating random data... "; 
+dd if=/dev/urandom of=/tmp/rand bs=1 count=1000000 >/dev/null 2>&1
+echo "done."; echo
 
-for RAW in raw /usr/share/dict/words #/tmp/rand
+for RAW in raw /usr/share/dict/words /tmp/rand
 do
   echo -en "original file: "; ls -ltH $RAW
 
 # encode/decode with various max #dictionary entries
-  for D in 1000 10000 100000 999999
+  for D in 512 4096 8192 16384 100000 999999
   do
     $V $H -e -i $RAW -o cwh -D $D
     $V $H -d -i  cwh -o bak -D $D
